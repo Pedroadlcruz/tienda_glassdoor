e7719716b38c524cbcd3633959dd56b86c6cef74
Pedro A. De la Cruz <pedrodlcruz@coolhunterpartners.com>
2025-08-04 13:07:18 -0400

    🚧 Refactor service locator to include AuthBloc and ProductsBloc; replace Logout use case with AuthUseCases and ProductsUseCases

--- lib/core/config/di/service_locator.dart
+++ lib/core/config/di/service_locator.dart
@@ -3,12 +3,17 @@ import 'package:get_it/get_it.dart';
 
 import '../../../features/auth/data/repositories/auth_repository_impl.dart';
 import '../../../features/auth/domain/repositories/auth_repository.dart';
+import '../../../features/auth/domain/usecases/auth_usecases.dart';
 import '../../../features/auth/domain/usecases/login_with_email.dart';
 import '../../../features/auth/domain/usecases/login_with_google.dart';
-import '../../../features/auth/domain/usecases/logout.dart';
 import '../../../features/auth/domain/usecases/register_with_email.dart';
+import '../../../features/auth/presentation/blocs/auth/auth_bloc.dart';
 import '../../../features/auth/presentation/blocs/login/login_bloc.dart';
 import '../../../features/auth/presentation/blocs/register/register_bloc.dart';
+import '../../../features/products/data/repositories/fake_products_repository_impl.dart';
+import '../../../features/products/domain/repositories/products_repository.dart';
+import '../../../features/products/domain/usecases/products_usecases.dart';
+import '../../../features/products/presentation/blocs/products/products_bloc.dart';
 import '../../services/network/network_info.dart';
 import '../router/app_router_refresh_notifier.dart';
 
@@ -25,16 +30,22 @@ Future<void> setUpServiceLocator() async {
   sl.registerFactory(
     () => RegisterBloc(sl<RegisterWithEmail>(), sl<LoginWithGoogle>()),
   );
+  sl.registerFactory(() => AuthBloc(sl<AuthUseCases>()));
+  sl.registerFactory(() => ProductsBloc(sl<ProductsUseCases>()));
 
   //! Use cases
   sl.registerLazySingleton(() => LoginWithEmail(sl<AuthRepository>()));
   sl.registerLazySingleton(() => LoginWithGoogle(sl<AuthRepository>()));
   sl.registerLazySingleton(() => RegisterWithEmail(sl<AuthRepository>()));
-  sl.registerLazySingleton(() => Logout(sl<AuthRepository>()));
+  sl.registerLazySingleton(() => AuthUseCases(sl<AuthRepository>()));
+  sl.registerLazySingleton(() => ProductsUseCases(sl<ProductsRepository>()));
 
   //! Repositories
 
   sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
+  sl.registerLazySingleton<ProductsRepository>(
+    () => FakeProductsRepositoryImpl(),
+  );
 
   //!Services
 
